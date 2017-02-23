595b47ad65ab168e894d7b4e64868e492234b436
Jane Poho <jane@chriss-MacBook-Air.local>
2017-02-22 18:29:44 -0700

    Improved CSS + HTML

--- js/script.js
+++ js/script.js
@@ -1,76 +1,99 @@
-<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+/* JS TO MAKE LITY VIDEO OPEN */
 
-<script type="text/javascript">
-    $('header .fa').on('click', function() {
-        $('header nav').toggleClass('show');
-    });
-
-    $(window).on('scroll', function() {
-        var y = $(this).scrollTop();
-
-        $('header nav').removeClass('show');
-
-        if (y > 50)
-            $('header').addClass('scroll');
-        else $('header').removeClass('scroll');
-    });
-</script>
-
-<!--JS TO MAKE VIDEO OPEN -->
-<script src="vendor/jquery.js"></script>
-<script src="dist/lity.js"></script>
+src="vendor/jquery.js"
+src="dist/lity.js"
 
 
-<!-- LOGO 30% SMALLER ON SCROLL -->
-
-<script type="text/javascript">
-    $(document).on("scroll", function() {
-        if ($(document).scrollTop() > 100) {
-            $("header").addClass("smaller");
-        } else {
-            $("header").removeClass("smaller");
-        }
-    });
-</script>
 
 
-<script>
-    $(document).ready(function() {
-        $(document).on("scroll", onScroll);
+/* ADDS WHITE BACKGROUND TO HEADER WHEN SCROLLED PAST 50PX */
 
-        //smoothscroll
-        $('a[href^="#"]').on('click', function(e) {
-            e.preventDefault();
-            $(document).off("scroll");
+        $(window).on('scroll', function() {
+            var y = $(this).scrollTop();
 
-            $('header nav a').each(function() {
-                $(this).removeClass('active');
-            })
-            $(this).addClass('active');
+            $('header nav').removeClass('show');
 
-            var target = this.hash,
-                menu = target;
-            $target = $(target);
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top + 2
-            }, 500, 'swing', function() {
-                window.location.hash = target;
-                $(document).on("scroll", onScroll);
-            });
+            if (y > 50)
+                $('header').addClass('scroll');
+            else $('header').removeClass('scroll');
         });
-    });
 
-    function onScroll(event) {
-        var scrollPos = $(document).scrollTop();
-        $('header nav a').each(function() {
-            var currLink = $(this);
-            var refElement = $(currLink.attr("href"));
-            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                $('header nav a').removeClass("active");
-                currLink.addClass("active");
+
+/* LOGO 30% SMALLER ON SCROLL */
+
+        $(document).ready(function() {
+
+        $(document).on("scroll", function() {
+            if ($(document).scrollTop() > 100) {
+                $("header").addClass("smaller");
             } else {
-                currLink.removeClass("active");
+                $("header").removeClass("smaller");
             }
         });
-    }
-</script>
+      });
+
+
+/* NAV BAR LIST ITEMS ACTIVATE */
+
+        $(document).ready(function() {
+            $(document).on("scroll", onScroll);
+
+            //smoothscroll
+            $('a[href^="#"]').on('click', function(e) {
+                e.preventDefault();
+                $(document).off("scroll");
+
+                $('header nav a').each(function() {
+                    $(this).removeClass('active');
+                })
+                $(this).addClass('active');
+
+                var target = this.hash,
+                    menu = target;
+                $target = $(target);
+                $('html, body').stop().animate({
+                    'scrollTop': $target.offset().top + 2
+                }, 500, 'swing', function() {
+                    window.location.hash = target;
+                    $(document).on("scroll", onScroll);
+                });
+            });
+        });
+
+        function onScroll(event) {
+            var scrollPos = $(document).scrollTop();
+            $('header nav a').each(function() {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                    $('header nav a').removeClass("active");
+                    currLink.addClass("active");
+                } else {
+                    currLink.removeClass("active");
+                }
+            });
+        }
+
+
+/* FORM LABEL ANIMATES*/
+
+    $(document).ready(function() {
+
+$('input').on('blur', function(){
+  if( !$(this).val() == "" ){
+    $(this).next().addClass('stay');
+  } else {
+    $(this).next().removeClass('stay');
+  }
+});
+                  });
+
+  $(document).ready(function() {
+    $('textarea').on('blur', function(){
+      if( !$(this).val() == "" ){
+        $(this).next().addClass('stay');
+        } else {
+            $(this).next().removeClass('stay');
+            }
+          });
+      });
